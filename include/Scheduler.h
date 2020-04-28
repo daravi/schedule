94c4e68fcef848b2a5706c1d53634a65b60fdaea
Puya Daravi <puya@motionmetrics.com>
2020-04-28 12:17:07 -0700

    Some clean-ups

--- include/Scheduler.h
+++ include/Scheduler.h
@@ -15,22 +15,23 @@
 
 namespace sched
 {
-
 class Scheduler
 {
-    using sys_time_t = std::chrono::system_clock::time_point;
+    using steady_time_t = std::chrono::steady_clock::time_point;
+    
     struct Entry
     {
         unsigned int id;
-        sys_time_t next_call;
+        steady_time_t next_call;
         std::function<void()> callback{ nullptr };
         bool repeated{ false };
         std::optional<std::chrono::microseconds> interval{ std::nullopt };
     };
+    
 public:
     Scheduler();
     ~Scheduler();
-
+    
     template <
         class Fn,
         class... Args,
@@ -39,7 +40,7 @@ public:
             && std::is_same<std::invoke_result_t<Fn, Args...>, void>::value
         >
     >
-    unsigned int registerCall(const std::chrono::system_clock::time_point& start, Fn&& f, Args&&... args)
+    unsigned int registerCall(const std::chrono::steady_clock::time_point& start, Fn&& f, Args&&... args)
     {
         insert(Entry{
             ++_last_id,
@@ -50,7 +51,7 @@ public:
         });
         return _last_id;
     }
-
+    
     template <
         class Rep,
         class Period,
@@ -61,18 +62,18 @@ public:
             && std::is_same<std::invoke_result_t<Fn, Args...>, void>::value
         >
     >
-    unsigned int registerRepeatedCall(const std::chrono::system_clock::time_point& start, const std::chrono::duration<Rep, Period>& dur, Fn&& f, Args&&... args)
+    unsigned int registerRepeatedCall(const std::chrono::steady_clock::time_point& start, const std::chrono::duration<Rep, Period>& dur, Fn&& f, Args&&... args)
     {
         insert(Entry{
             ++_last_id,
             start,
             std::bind(std::forward<Fn>(f), std::forward<Args>(args)...),
             true,
-            std::chrono::duration_cast<std::chrono::nanoseconds>(dur)
+            std::chrono::duration_cast<std::chrono::microseconds>(dur)
         });
         return _last_id;
     }
-
+    
     template <
         class Rep,
         class Period,
@@ -85,23 +86,26 @@ public:
     >
     unsigned int registerRepeatedCall(const std::chrono::duration<Rep, Period>& dur, Fn&& f, Args&&... args)
     {
-        return registerRepeatedCall(std::chrono::system_clock::now(), dur, std::forward<Fn>(f), std::forward<Args>(args)...);
+        return registerRepeatedCall(std::chrono::steady_clock::now(), dur, std::forward<Fn>(f), std::forward<Args>(args)...);
     }
-
+    
+    void unregisterCall(unsigned int call_id);
+    
 private:
     void execution_thread();
     void insert(Entry&& entry);
+    
     using iter_t = std::vector<Entry>::iterator;
     static iter_t find_insert_location(iter_t begin, iter_t end, const Entry& entry);
     
     unsigned int _last_id{ 0 };
     std::vector<Entry> _schedule;
+    
     std::thread _execution_thread;
-    std::vector<std::future<void>> _call_futures;
-    std::condition_variable_any _exectution_cv;
+    std::vector<std::future<void>> _call_futures; /* TODO PUYA: Do we need std::future or std::thread is enough? */
+    std::condition_variable _exectution_cv;
     std::mutex _executionMtx;
     bool _running{ false };
-    bool _new_entry_available{ false };
+    bool _schedule_updated{ false };
 };
-
 }
--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -1,12 +1,9 @@
 #include "Scheduler.h"
 
-#include <iostream>
-
 using namespace std::chrono_literals;
 
 namespace sched
 {
-
 Scheduler::Scheduler()
 {
     _running = true;
@@ -21,67 +18,89 @@ Scheduler::~Scheduler()
     }
     _exectution_cv.notify_one();
     _execution_thread.join();
+    
     _call_futures.clear(); // blocks until all started executions end
+}
+
+void Scheduler::unregisterCall(unsigned int call_id)
+{
+    {
+        std::unique_lock lock{_executionMtx};
+        auto entry_it = std::find_if(_schedule.begin(), _schedule.end(),
+        [&](const Entry& current_entry) -> bool
+        {
+            return current_entry.id == call_id;
+        });
+        
+        _schedule.erase(entry_it);
+        _schedule_updated = true;
+    }
+    _exectution_cv.notify_one();
 }
 
 void Scheduler::execution_thread()
 {
     while (_running)
     {
+        std::unique_lock lock{_executionMtx};
         if (_schedule.empty())
         {
-            std::unique_lock lock{_executionMtx};
-            _exectution_cv.wait(lock, [&](){ return _new_entry_available || !_running; });
-            _new_entry_available = false;
+            _exectution_cv.wait(lock, [&](){ return _schedule_updated || !_running; });
+            _schedule_updated = false;
             continue;
         }
-        std::unique_lock lock{_executionMtx};
-        auto no_timeout = _exectution_cv.wait_until(lock, _schedule.front().next_call, [&](){ return _new_entry_available || !_running; });
+        
+        auto next_call = _schedule.front().next_call;
+        auto no_timeout = _exectution_cv.wait_until(lock, next_call, [&](){ return _schedule_updated || !_running; });
+        
         if (no_timeout)
         {
-            _new_entry_available = false;
+            _schedule_updated = false;
             continue;
         }
-        auto entry = _schedule.begin();
+        
+        auto entry_it = _schedule.begin();
+        
         _call_futures.emplace_back(std::async(std::launch::async,
-            [&]()
+            [entry{ *entry_it }]()
             {
-                entry->callback();
+                entry.callback();
             }
         ));
-        if (entry->repeated)
+        
+        if (entry_it->repeated)
         {
-            entry->next_call += entry->interval.value();
+            entry_it->next_call += entry_it->interval.value();
             if (_schedule.size() == 1) continue;
-            auto it = find_insert_location(entry + 1, _schedule.end(), *entry);
+            
+            auto it = find_insert_location(entry_it + 1, _schedule.end(), *entry_it);
             std::rotate(_schedule.begin(), _schedule.begin() + 1, it);
         }
         else
         {
-            _schedule.erase(entry);
+            _schedule.erase(entry_it);
         }
     }
 }
 
 Scheduler::iter_t Scheduler::find_insert_location(iter_t begin, iter_t end, const Entry& entry)
 {
-    return std::lower_bound(begin, end, entry, 
-        [&](const Entry& entry, const Entry& current_entry) -> bool 
-        { 
-            return entry.next_call < current_entry.next_call;
+    return std::lower_bound(begin, end, entry,
+        [](const Entry& current_entry, const Entry& entry) -> bool
+        {
+            return current_entry.next_call < entry.next_call;
         }
     );
 }
 
 void Scheduler::insert(Entry&& entry)
-{   
+{
     {
         std::scoped_lock lock{_executionMtx};
         auto it = find_insert_location(_schedule.begin(), _schedule.end(), entry);
         _schedule.insert(it, std::forward<Entry>(entry));
-        _new_entry_available = true;
+        _schedule_updated = true;
     }
     _exectution_cv.notify_one();
 }
-
 }
